03aa7a9a196dd74e996b51f56e6130187843f231
coelhohubx-png <coelhohubx@gmail.com>
2026-07-02 22:24:05 -0300

    Update Lua script to use new loader URL

--- .lua
+++ .lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luacrack.site/files/v4/loaders/8a34ade524aac301331777f2a70892fc.lua"))()
+loadstring(game:HttpGet("https://obfuscatorhub.onrender.com/api/f7v41etr"))()
